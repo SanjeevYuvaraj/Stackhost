b2d1429412e408c8230a880642024661eb57dfec
SanjeevYuvaraj <7bsanjeev@gmail.com>
2026-02-24 22:00:05 +0530

    Update config.py

--- stackhost/config.py
+++ stackhost/config.py
@@ -1,6 +1,6 @@
-API_ID = 123456
-API_HASH = "API_HASH"
-BOT_TOKEN = "BOT_TOKEN"
+API_ID = 18990697
+API_HASH = "f4815b9a16cb03c2f5eabe8db1cb0903"
+BOT_TOKEN = "8561787736:AAEZ6ASXkxGS80dh7rpX9NvDdLwRrWJLQwo"
 
-MONGO_URI = "mongodb://localhost:27017"
-OWNER_ID = 123456789
+MONGO_URI = "mongodb+srv://publicDB:publicDBbyKira@public.twckcqf.mongodb.net/?retryWrites=true&w=majority"
+OWNER_ID = 6651534688
